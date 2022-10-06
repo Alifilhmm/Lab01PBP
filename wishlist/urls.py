6c084a805705f442d8b1882d19224fc0b5bae0c4
alifilhmm <alifilhmm@gmail.com>
2022-10-06 16:39:39 +0700

    lab 5 masi error nt

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -7,6 +7,9 @@ from wishlist.views import show_xml_by_id #sesuaikan dengan nama fungsi yang dib
 from wishlist.views import register #sesuaikan dengan nama fungsi yang dibuat
 from wishlist.views import login_user #sesuaikan dengan nama fungsi yang dibuat
 from wishlist.views import logout_user #sesuaikan dengan nama fungsi yang dibuat
+from wishlist.views import show_wishlist_ajax
+from wishlist.views import new_wishlist_item
+
 
 app_name = 'wishlist'
 
@@ -19,4 +22,6 @@ urlpatterns = [
     path('register/', register, name='register'), #sesuaikan dengan nama fungsi yang dibuat
     path('login/', login_user, name='login'), #sesuaikan dengan nama fungsi yang dibuat
     path('logout/', logout_user, name='logout'), #sesuaikan dengan nama fungsi yang dibuat
+    path("ajax/", show_wishlist_ajax, name="show_wishlist_ajax"),
+    path("ajax/submit/", new_wishlist_item, name="new_wishlist_item"),
 ]
